f5cfdaf1b529b4c04156aebbe2a2b6958087749c
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-09-27 18:20:10 -0400

    maps configured

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+
+
+function Header () {
+return <div className="Nav-bar">
+                <h1 className="Nav-title">Safehive</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Incident chart</li>
+                    <li>About us</li>
+                </ul>
+            </nav>
+        </div>
+}
+
+export default Header;
